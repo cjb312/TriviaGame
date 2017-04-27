04c7897c5c4bd7050a516778487bb5b38699466c
Chris Barnes <cjbarnes312@gmail.com>
2017-04-27 18:19:15 -0400

    Finished CSS and timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,10 @@
 $(function () {
-// variables
+
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
-// end variables
 
-//hide and display divs
-$("#startButton").click(function() {
-    $("#startPage").hide();
-    $("#triviaPage").show();
-});
-
-$("#doneButton").click(function() {
-    clearInterval(interval);
-    $("#triviaPage").hide();
-    $("#resultsPage").show();
-});
-//end hide and display divs
-
-//timer functions
-var counter = 0;
-
-var interval = setInterval(function() {
-    counter++;
-    
-    if (counter == 60) {
-        // Display a login box
-        clearInterval(interval);
-        $('#timeUpModal').modal('show');
-        $("#modalButton").click(function() {
-            $("#doneButton").click();
-        });
-    }
-}, 1000);
-// end timer functions
-
-//function that checks the answers
+// checks the answers
 function checkAnswer() {
     $("input:checked").each(function(){
         var checkedValue = $(this).val()
@@ -47,7 +16,6 @@ function checkAnswer() {
     })
 
     unanswered = 5 - (correctAnswers + incorrectAnswers)
-    //display results onto html results page
     $("#correctAnswers").html(correctAnswers);
     $("#incorrectAnswers").html(incorrectAnswers);
     $("#unanswered").html(unanswered);
@@ -56,6 +24,31 @@ function checkAnswer() {
 $(document).on("click", "#doneButton", function(){
     checkAnswer();
 })
-// end checkAnswer function
-
 });
+
+// Display and hide start and results page
+$("#startButton").click(function() {
+    $("#startPage").hide();
+    $("#triviaPage").show();
+});
+
+$("#doneButton").click(function() {
+    clearInterval(interval);
+    $("#triviaPage").hide();
+    $("#resultsPage").show();
+});
+
+//time remaining function
+var counter = 0;
+var timeRemaining = 60;
+var interval = setInterval(function() {
+    counter++;
+    $('#timeRemaining').html(timeRemaining - counter);
+    if (counter == 60) {
+        clearInterval(interval);
+        $('#timeUpModal').modal('show');
+        $("#modalButton").click(function() {
+            $("#doneButton").click();
+        });
+    }
+}, 1000);
